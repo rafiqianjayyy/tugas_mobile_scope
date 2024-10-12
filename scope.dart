f6b5a5f1ce38e76be58ad2630bb46cecb5710470
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:47:11 +0700

    Create scope.dart

---
+++ scope.dart
@@ -0,0 +1,12 @@
+//INI ADALAH CONTOH SCOPE
+// void main(){
+//   var nama = 'Rafiqi';
+
+//   void halo(){
+//     var hai = 'hai ges aku $nama';
+//     print(hai);
+//   }
+
+//   halo();
+//   // print(hai); error karena memanggil ini di luar dari fungsinya
+// }
